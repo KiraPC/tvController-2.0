46dd590c8b68f203c1e2cbe0a21277f48328d4bf
Pasquale Carbone <p.carbone@accenture.com>
2019-12-30 16:11:26 +0100

    added client config

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
+// @ts-ignore
+const config = require('../config/client.json');
 const TvClientInterface = require('./lib/TvClientInterface');
 
 // eslint-disable-next-line no-new
-new TvClientInterface('wss://tranquil-stream-30273.herokuapp.com', '123');
+new TvClientInterface(config.endpoint, config.deviceId);
